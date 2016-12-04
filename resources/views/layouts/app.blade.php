9310f3b92c9926f3d582062d9ee66ad9729ceaf5
mohsin <mohsin.cse.45@gmail.com>
2016-12-04 19:44:50 +0600

    hide register

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -61,7 +61,7 @@
                     <!-- Authentication Links -->
                     @if (Auth::guest())
                         <li><a href="{{ url('/login') }}">Login</a></li>
-                        <li><a href="{{ url('/register') }}">Register</a></li>
+                        {{--<li><a href="{{ url('/register') }}">Register</a></li>--}}
                     @else
                         <li><a href="{{ url('/home') }}">Home</a></li>
                         <li><a href="{{ url('/class') }}">Class</a></li>
